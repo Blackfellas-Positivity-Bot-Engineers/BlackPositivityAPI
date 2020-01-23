f9738f8de092602ca03eef171fb96b9940c0b863
Christopher Smith <csmith4work@gmail.com>
2020-01-23 09:10:59 -0600

    Passing in env to dockerfile.

--- BlackPositivity.Api/Startup.cs
+++ BlackPositivity.Api/Startup.cs
@@ -19,15 +19,14 @@ namespace BlackPositivity.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        public Startup(IConfiguration configuration, IHostEnvironment env)
         {
             Configuration = configuration;
             Environment = env;
-            Console.WriteLine("Current Environment - " + env.EnvironmentName);
         }
 
         public IConfiguration Configuration { get; }
-        public IWebHostEnvironment Environment { get; }
+        public IHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -47,6 +46,8 @@ namespace BlackPositivity.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            Console.WriteLine("Current Environment - " + Environment.EnvironmentName);
             app.UseSwagger();
 
             app.UseSwaggerUI(c =>
