361712d27730e9caacfb1f5a0e6d2938219d2f6a
Christopher Smith <csmith4work@gmail.com>
2020-02-04 08:33:59 -0600

    Prod build.:

---
+++ prodbuild.sh
@@ -0,0 +1,23 @@
+# Initializing Variables
+_env=$1
+_imagetag=blackpositivityapi-$_env
+echo $_imagetag
+
+#Stop container
+docker stop $_imagetag
+
+#Remove container
+docker rm --force $_imagetag
+
+#Remove image
+docker rmi $_imagetag
+
+#Build new image
+docker build --no-cache -t "$_imagetag" .
+
+#Run new image
+docker run -e ASPNETCORE_ENVIRONMENT=$_env -d -p 3006:80 --name "$_imagetag" $_imagetag
+
+echo $_imagetag new version up! 
+
+exit 0
